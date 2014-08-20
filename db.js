18be19733bf0740c251cc016b65a019f4df8cb5e
James H. Edwards <james.howe.edwards@gmail.com>
2014-08-20 09:00:05 -0700

    change to bcrypt-nodejs

--- db.js
+++ db.js
@@ -1,33 +1,29 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
-var bcrypt = require('bcrypt');
+var bcrypt = require('bcrypt-nodejs');
 var SALT_WORK_FACTOR = 10;
 
 var Member = new Schema({
-	name: String,
-	username: { type: String, required: true, index: {unique: true} },
-	password: {type: String, required: true},
-	email: String,
-	joined: Date,
-	bio: String,
-	genres: Array,
-	authors: Array,
-	following: Array
+  name: String,
+  username: { type: String, required: true, index: {unique: true} },
+  password: {type: String, required: true},
+  email: String,
+  joined: Date,
+  bio: String,
+  genres: Array,
+  authors: Array,
+  following: Array
 });
 
 Member.pre('save', function(next) {
-	var mem = this;
-	if(!mem.isModified('password')) return next();
+  var member = this;
+  if(!member.isModified('password')) return next();
 
-	bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+  bcrypt.hash(member.password, null, null, function(err, hash) {
     if (err) return next(err);
 
-    bcrypt.hash(mem.password, salt, function(err, hash) {
-      if (err) return next(err);
-
-      mem.password = hash;
-      next();
-    });
+    member.password = hash;
+    next();
   });
 });
 
@@ -43,72 +39,72 @@ var member = mongoose.model('member', Member);
 
 
 var BookReview = new Schema({
-	title: String,
-	author: String,
-	url: String,
-	review: String,
-	reviewby: String,
-	genre: String,
-	tags: Array,
-	datetime: Date
+  title: String,
+  author: String,
+  url: String,
+  review: String,
+  reviewby: String,
+  genre: String,
+  tags: Array,
+  datetime: Date
 });
 
 var review = mongoose.model('review', BookReview);
 
 var BookList = new Schema({
-	name: String,
-	items: Array,
-	listby: String,
-	datetime: Date
+  name: String,
+  items: Array,
+  listby: String,
+  datetime: Date
 })
 
 var list = mongoose.model('list', BookList);
 
 var Note = new Schema({
-	name: String,
-	noteby: String,
-	content: String,
-	tags: Array,
-	datetime: Date
+  name: String,
+  noteby: String,
+  content: String,
+  tags: Array,
+  datetime: Date
 })
 
 var note = mongoose.model('note', Note);
 
 var Action = new Schema({
-	DateTime: Date,
-	whodunnit: String,
-	category: String, //note, review, list, request
-	actionRef: String,
-	author: String,
-	genre: String,
-	description: String
+  DateTime: Date,
+  whodunnit: String,
+  category: String, //note, review, list, request
+  actionRef: String,
+  author: String,
+  genre: String,
+  description: String
 
 })
 
 var action = mongoose.model('action', Action);
 
 //var newsItem = new Schema({
-//	userID: String,
-//	actionID: String,
-//	Relevancy: Date
+//  userID: String,
+//  actionID: String,
+//  Relevancy: Date
 //})
 
 //var newsItem = mongoose.model('newsItem', newsItem);
 
 var Message = new Schema({
-	title: String,
-	sender: String,
-	recipient: Array,
-	content: String,
-	senton: Date
+  title: String,
+  sender: String,
+  recipient: Array,
+  content: String,
+  senton: Date
 })
 
 var Quest = new Schema({
-	madeby: String,
-	name: String,
-	content: String,
-	tags: Array,
-	datetime: Date
+  madeby: String,
+  name: String,
+  content: String,
+  tags: Array,
+  datetime: Date
 })
 
 var quest = mongoose.model('quest', Quest);
